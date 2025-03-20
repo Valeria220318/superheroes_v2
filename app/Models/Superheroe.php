901447be179b8ff55e71116384d1641235a13a01
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-20 00:36:17 -0600

    Update Superheroe.php

--- app/Models/Superheroe.php
+++ app/Models/Superheroe.php
@@ -3,9 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Superheroe extends Model
 {
+    use SoftDeletes; // Habilita el borrado lógico
+
     protected $table = 'superheroes';
-    protected $fillable = ['nombre_real', 'alias', 'foto', 'informacion'];
+
+    // Campos que se pueden asignar masivamente
+    protected $fillable = ['nombre_real', 'alias', 'foto', 'informacion', 'activo'];
+
+    // Indica el campo que usará SoftDeletes
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Scope para filtrar los superhéroes activos
+     */
+    public function scopeActivos($query)
+    {
+        return $query->where('activo', true);
+    }
 }
+
